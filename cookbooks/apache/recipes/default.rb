f4861c86ff21f968a46d033381809982d929004b
Vamshi <techygeek001@gmail.com>
2017-02-17 00:34:52 +0000

    Used attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,8 +4,14 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+if node['platform_family'] == "rhel"
+	platform = 'httpd'
+elsif node ['platform_family'] == "debian"
+	platform = 'apache2'
+end
+
 package "apache" do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
@@ -13,5 +19,4 @@ service "apache" do
 	service_name 'httpd'
 	action [:enable, :start]
 end
-
 
---
+++ cookbooks/apache/recipes/motd.rb
@@ -0,0 +1,6 @@
+hostname = node['hostname']
+
+file '/etc/motd' do
+	content "Hostname is  #{hostname}"
+end
+
